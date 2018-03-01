bfdf39980311adfff1967b609e1759673eb645cf
Peter deHaan <peter@deseloper.com>
2018-02-28 16:13:32 -0800

    Tweak linter

--- schema-lint.js
+++ schema-lint.js
@@ -1,16 +1,13 @@
 import {validate} from './schema';
 import {presetColors} from './src/lib/constants';
-import defaultColors from './src/lib/default-colors.json';
 
 (async () => {
   presetColors.forEach(async (colors, idx) => {
     const prefix = `presetColors[${idx}]`;
     try {
       const result = await validate(colors);
-      console.log(prefix, 'success!', /* JSON.stringify(result) */);
+      console.log(prefix, 'success!' /*, JSON.stringify(result) */);
     } catch (err) {
-      // console.error(prefix, JSON.stringify(err.errors, null, 2));
-      // console.error(prefix);
       err.errors.forEach(error => {
         switch (error.keyword) {
           case 'additionalProperties':
